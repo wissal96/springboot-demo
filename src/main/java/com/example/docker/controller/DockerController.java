5de4215a7e0976685cd1d97a491e6a0de0ceb43f
wissal96 <halhan820@gmail.com>
2026-03-02 15:25:18 +0100

    change the code

--- src/main/java/com/example/docker/controller/DockerController.java
+++ src/main/java/com/example/docker/controller/DockerController.java
@@ -7,13 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DockerController {
     @GetMapping("/docker")
-    public String dockerDemo(){
+    public String dockerDemo() {
         return "Dockerizing spring boot application";
     }
 }
-#git init
-#git add README.md
-#git commit -m "first commit"
-#git branch -M main
-#git remote add origin https://github.com/wissal96/springboot-demo.git
-#git push -u origin main
